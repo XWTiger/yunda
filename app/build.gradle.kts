696be2073f527e44784e9afdd75b95d1a22d29aa
xiaweihu <455362630@qq.com>
2024-07-03 21:53:38 +0800

    [tiger] add version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.tiger.yunda"
         minSdk = 29
         targetSdk = 34
-        versionCode = 10014
-        versionName = "1.0.0.1.4"
+        versionCode = 10015
+        versionName = "1.0.0.1.5"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
