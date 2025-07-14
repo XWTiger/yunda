bbe02db8fdebc0954fec2c5cd6b6f37b9137b90c
xiaweihu <455362630@qq.com>
2025-07-14 22:33:32 +0800

    [tiger] 修复派发下拉数据重复问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 10021
-        versionName = "1.0.0.2.2"
+        versionName = "1.0.0.2.3"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
